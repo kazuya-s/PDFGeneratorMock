2524d234251a7128870e261d08ae862aa93cfa4d
Kazuya Shindome <kshindome@appirio.com>
2014-08-07 10:30:11 +0900

    return sample pdf

---
+++ PDFGeneratorMock/Default.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace PDFGeneratorMock
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // 遅延を発生させる
+            System.Threading.Thread.Sleep(5000);
+
+            Context.Response.Buffer = false;
+            Response.ContentType = @"application/pdf";
+
+
+            byte[] bs = Properties.Resources.SamplePdf;
+
+            if (Context.Response.IsClientConnected)
+            {
+                Context.Response.OutputStream.Write(bs, 0, bs.Length);
+                Context.Response.Flush();
+            }
+        }
+    }
+}
