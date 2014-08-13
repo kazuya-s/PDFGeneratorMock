220a02521c2be52ac2e403cec6bf84ed0549e9e6
Kazuya Shindome <kshindome@appirio.com>
2014-08-13 14:50:15 +0900

    Add Dynamic mode

--- PDFGeneratorMock/Default.aspx.cs
+++ PDFGeneratorMock/Default.aspx.cs
@@ -6,20 +6,49 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+using PdfSharp.Drawing;
+using PdfSharp.Pdf;
+using PdfSharp.Pdf.IO;
+using PdfSharp.Drawing.Layout;
+
 namespace PDFGeneratorMock
 {
     public partial class _Default : Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            // 遅延を発生させる
-            System.Threading.Thread.Sleep(5000);
+            byte[] bs = null;
 
-            Context.Response.Buffer = false;
-            Response.ContentType = @"application/pdf";
+            if (Context.Request["mode"] == "dynamic")
+            {
+                // PDFを動的に作る
+                using (MemoryStream stream = new MemoryStream())
+                {
+                    PdfDocument document = new PdfDocument();
+                    PdfPage page = document.AddPage();
+
+                    // アクセスのあった文字列を表示
+                    XGraphics gfx = XGraphics.FromPdfPage(page);
+                    XFont font = new XFont("Times New Roman", 10, XFontStyle.Regular);
+                    XTextFormatter tf = new XTextFormatter(gfx);
+                    XRect rect = new XRect(40, 100, 500, 200);
+                    tf.DrawString("URL = " + Context.Request.Url.ToString(), font, XBrushes.Black, rect, XStringFormats.TopLeft);
+
+                    document.Save(stream, true);
+                    bs = stream.ToArray();
+                }
+            }
+            else
+            {
+                // 静的なPDFを返す
+                System.Threading.Thread.Sleep(5000);
+
+                Context.Response.Buffer = false;
+                Response.ContentType = @"application/pdf";
 
 
-            byte[] bs = Properties.Resources.SamplePdf;
+                bs = Properties.Resources.SamplePdf;
+            }
 
             if (Context.Response.IsClientConnected)
             {
